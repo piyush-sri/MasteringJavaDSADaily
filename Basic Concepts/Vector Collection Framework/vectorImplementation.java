1633560924f5c54b44ade7595761e275983879b0
Piyush Srivastava <67270567+piyush-sri@users.noreply.github.com>
2022-11-16 23:09:42 +0530

    Create vectorImplementation.java
    Learning Vector collection framework in java

---
+++ Basic Concepts/Vector Collection Framework/vectorImplementation.java
@@ -0,0 +1,20 @@
+//Vector Data Structure---Allow Duplicates, Hetrogeneous element, allowed null values.
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+ //Vector containing hetrogeneous network
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Vector firstVector=new Vector();
+		//to add something in your vector use add()  Method
+        firstVector.add(1);
+        firstVector.add("Piyush");
+        firstVector.add(113);
+        firstVector.add(11.2);
+        System.out.println("The Vector is = "+ firstVector);
+	}
+}
