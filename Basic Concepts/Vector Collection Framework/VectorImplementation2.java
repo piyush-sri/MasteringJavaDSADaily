71b3367ad6f574ed3431edb60bd0171798e20bbc
Piyush Srivastava <67270567+piyush-sri@users.noreply.github.com>
2022-11-16 23:14:03 +0530

    Create VectorImplementation2.java
    Vector with type safety

---
+++ Basic Concepts/Vector Collection Framework/VectorImplementation2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 
+    //	Creating a vector instance which is type safe
+    // This can contain only String elements.
+    //If any other type of element is added, it will result in compile //time error.
+    Vector<String> SecondVector= new Vector<String>();
+		//to add something in your vector use add()  Method
+        SecondVector.add("Mr.");
+        SecondVector.add("Piyush");
+        SecondVector.add("Learning");
+        SecondVector.add("Vector");
+        System.out.println("The Vector is = "+ SecondVector);
+	}
+}
